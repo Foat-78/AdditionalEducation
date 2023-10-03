12ac0ece41874f3b84e5d4ef7d14bdab676a6f92
Foat <Khisamov.foat@mail.ru>
2023-10-03 15:21:56 +1000

    Старт и финиш String и StringBuilder

---
+++ src/Task3/StatUmnee16.java
@@ -0,0 +1,19 @@
+package Task3;
+// Время потраченное на выполнение метода с классом String и StringBuilder
+public class StatUmnee16 {
+    public static void main(String[] args) {
+
+        var s = System.currentTimeMillis();
+//          String str = "";
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 1_000_000; i++) {
+//           str += "+";
+            sb.append("+");
+        }
+        System.out.println(System.currentTimeMillis() - s);
+//            System.out.println(str);
+        System.out.println(sb);
+    }
+}
+
+
